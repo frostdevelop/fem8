8af6d3e0f01c117283bfee475926171547e0f68a
Pacifiky <142683933+Pacifiky@users.noreply.github.com>
2025-01-17 16:48:12 -0700

    indev raycaster

---
+++ raycaster.asm
@@ -0,0 +1,43 @@
+#The FEMBOY RAYCASTER!
+#Let's have the map be 16x16.(256 bits and 32 bytes)
+#Can be 8x8 for beta
+#SYSTEM
+#-
+#1. Use dot product to figure out then use threstholds for each ray (getting vector from position to wall) then using the magnitude of each ray for distance?
+#2. Use a static rotation matrix to rotate vectors then walk on those vectors (accessing the memory of the int part) until meeting a wall (also use a counter for those steps for sizing).
+#-
+#Option 2 more efficient because rotating direction still req rotation matrix
+#Use ALU flag for out of bounds (and render wall there)
+#use 8 bit minifloat as well. or 4 bit . fixed point (16 lol);
+#Memory API: 
+#00 Function return 1
+#01 Function return 2
+#02 Arg 1
+#03 Arg 2
+#(Reg 03 is ret)
+#F0 X position 
+#F1 Y Position
+#F2 Directionx
+#F3 Directiony
+#Set fov to ~64 with rays every 4.
+#Init playerpos to 4x4
+LDA 04
+MAP F0
+MAP F1
+#Init map
+LDA 1C
+#Bit(00,01) Function 
+bit:
+LDA 01
+MVR 02
+bitlop:
+MVA 01 ;Bit#
+JZO endblop
+LDA 02
+MLT 02
+DEC 01
+JUP bitlop
+endblop:
+MVA 02
+AND 00
+JPP 03
