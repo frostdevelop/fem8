3fb4bc9bc23ebe6691b17521b8f4abaa274c2dc5
Pacifiky <142683933+Pacifiky@users.noreply.github.com>
2025-03-05 09:46:24 -0700

    Add, Mlt, and Div now supported globally

--- 16fdemo.asm
+++ 16fdemo.asm
@@ -1,51 +1,84 @@
 #232/27
 #INT:00
 #FRAC:01
+#CALC: 10
+#CALC: 11
+#M00: X
+#M01: Y
+#M02: Remainder (Dividing)
+#M03: Remainder (temp)
+#ADD
 LDA E8 ;232
+MAP 00
 MVR 00
 LDA 1B ;27
-DIV 00 ;8
-MVA 06 ;16
-#do a sal 1 and chkz loop for real situations
-SAL 03 ;Shift it right 3 (bits remain after div)
+MAP 01
+ADD 00
+MVA 04
+SAL 07
+SAR 07
 MVR 01
-LDA 27
-DIV 01
-MVA 01
-SAL 05 ;Get frac part
+#MLT
+MPA 00
+MVR 00
+MPA 01
+MLT 00
+MVA 07
 MVR 01
-#new it
+#DIV
+MPA 00
+MVR 00
+MPA 01
+DIV 00
+MVA 06 ;remain
+MAP 02
+CLR 02
+CLR 01
+LDA 08 ;the 8 bits
+MVR 03 ;Counter for shifts
+#Fractional loop
+fraclop:
+MPA 02
+SAL 01 ;Shift right
+MVR 02
+DEC 03
+MPA 01 ;27
+DIV 02 ;Divide and add
+MVA 02
+MAP 02
 MVA 06
-SAL 03 ;Shift right
+MAP 03
+MVA 03
 MVR 02
-LDA 27
-DIV 02
+JZO endslop
+JPL sarlop
+sallop:
+MPA 02
+SAL 01
+MAP 02
+DEC 02
 MVA 02
-SAL 02 ;Get frac part
+JZO endslop
+JUP sallop
+sarlop:
+MPA 02
+SAR 01
+MAP 02
+INC 02
+MVA 02
+JPL sarlop
+endslop:
+MPA 02
 ADD 01
-#new it
-MVA 06
-SAL 03 ;Shift right
+MPA 03
+JZO endflop
+MAP 02
+MVA 03
 MVR 02
-LDA 27
-DIV 02
+LDA 08
+ADD 02
 MVA 02
-SAR 01 ;Get frac part
-ADD 01
-#new it
-SAL 03 ;Shift right
-MVR 02
-LDA 27
-DIV 02
-MVA 02
-SAR 04 ;Get frac part
-ADD 01
-#new it
-SAL 03 ;Shift right
-MVR 02
-LDA 27
-DIV 02
-MVA 02
-SAR 07 ;Get frac part
-ADD 01
+JZO endflop
+JUP fraclop
+endflop:
 HLT
